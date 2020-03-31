28e1f691c15d403be7f4c3cfe8101a0abaf3b37f
Josh Long <josh@joshlong.com>
2020-03-31 00:13:07 -0700

    deploy

--- deploy.sh
+++ deploy.sh
@@ -1,44 +1,22 @@
 #!/bin/bash 
 
-
-# JBAKE_VERSION=2.6.4
-# echo "JBAKE_BIN=${JBAKE_BIN}."
-# echo "JBAKE_VERSION=${JBAKE_VERSION}"
-# curl -s "https://get.sdkman.io" | bash  || echo "SDKMAN is already installed..."
-
-# ls -la $HOME/.sdkman/
-# tree $HOME/.sdkman/
-
-# source "${HOME}/.sdkman/bin/sdkman-init.sh"
-# which sdk 
-# sdk version 
-# sdk install jbake $JBAKE_VERSION 
-# echo "SDKMAN_DIR is ${SDKMAN_DIR} "
-# source "${HOME}/.sdkman/bin/sdkman-init.sh"
-# OUTPUT_DIR=${GITHUB_WORKSPACE}/output
-# rm -rf ${OUTPUT_DIR}
-# jbake  . ${OUTPUT_DIR} --reset
-# cp -r content/media ${OUTPUT_DIR}/media
-# cp CNAME ${OUTPUT_DIR}/CNAME
-
- 
-
+JBAKE_VERSION=2.6.4
 BLOG_GITHUB_CHECKOUT=${GITHUB_WORKSPACE}/site 
-cd $BLOG_GITHUB_CHECKOUT && pwd 
+OUTPUT_DIR=${GITHUB_WORKSPACE}/output
 
-touch test.txt 
-echo `date` >> test.txt 
+curl -s "https://get.sdkman.io" | bash  || echo "SDKMAN is already installed..."
+source "${HOME}/.sdkman/bin/sdkman-init.sh"
+which sdk 
+sdk version 
+sdk install jbake $JBAKE_VERSION 
+source "${HOME}/.sdkman/bin/sdkman-init.sh"
+rm -rf ${OUTPUT_DIR}
+jbake  . ${OUTPUT_DIR} --reset
+cp -r content/media ${OUTPUT_DIR}/media
+cp CNAME ${OUTPUT_DIR}/CNAME
+
+cd $BLOG_GITHUB_CHECKOUT  
 git status
-git add test.txt 
-git commit -am 'test'
+git add * 
+git commit -am "new version $NOW "
 git push 
-
-# git checkout master 
-# pwd  
-# cp -r $OUTPUT_DIR/* . 
-# git add * 
-# git commit -am "new version $NOW "
-# echo "trying to do a git push for the repository "
-# cat .git/config 
-# git push 
-
